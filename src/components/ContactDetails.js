7bc4113a1644b11f293f699444ce58ffd4f3db43
RahulChinta <rahul.chinta@philips.com>
2021-07-10 17:33:30 +0530

    added search by index

--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Button from "./UI/Button";
 
 const ContactDetails = (props) => {
   const [currentNumberIndex, setCurrentNumberIndex] = useState(0);
+  const [error, setError] = useState();
+  const searchIndexRef = useRef();
   const dataLength = props.data[0]?.data?.length;
   const columnList = props.data.map((item, index) => {
     let content = <h1 key={index}>{item.data[currentNumberIndex]}</h1>;
@@ -16,8 +18,29 @@ const ContactDetails = (props) => {
     }
     return content;
   });
+
+  const setSearchIndex = () => {
+    const userSearchIndex = +searchIndexRef.current.value;
+    if (userSearchIndex < 0 || userSearchIndex > dataLength - 1) {
+      setError(`Please enter the number between 0 to ${dataLength - 1}`);
+    } else {
+      setError(null);
+      setCurrentNumberIndex(userSearchIndex);
+    }
+  };
+
   return (
     <div>
+      <div>
+        <label htmlFor="searchIndex">Search By Index Number</label>
+        <input id="searchIndex" ref={searchIndexRef} />
+        {error && <p className="error-text">{error}</p>}
+        <Button
+          onButtonClick={() => setSearchIndex()}
+          type="button"
+          value="Search"
+        />
+      </div>
       {columnList}
 
       {currentNumberIndex !== 0 && (
